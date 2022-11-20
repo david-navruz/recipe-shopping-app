65e05a99a4302c0f116c6fc91583b3ac0b235bef
david-navruz <selcuk-nevruz@hotmail.com>
2022-11-20 17:27:15 +0100

    updating the project

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,39 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes:Recipe[] = [
+    new Recipe('Cheese Pizza', 'How to make a cheese pizza',
+              'https://www.bettybossi.ch/rdbimg/bb_itku120801_0243a/bb_itku120801_0243a_r01_v005_x0010.jpg',
+              [
+                new Ingredient('Olives', 20),
+                new Ingredient('French cheese', 1)
+              ]),
+    new Recipe('Chicken Pizza', 'How to make a chicken pizza',
+                  'https://www.bettybossi.ch/rdbimg/bb_itku120801_0243a/bb_itku120801_0243a_r01_v005_x0010.jpg',
+                 [
+                  new Ingredient('Chicken Breast', 2),
+                  new Ingredient('Mozzarella', 2)
+                  ]),
+  ];
+
+   constructor(private slService: ShoppingListService) {}
+
+    // slice() method extracts a section of an array and returns a new array.
+    getRecipes() {
+      return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+      this.slService.addIngredients(ingredients);
+    }
+
+
+
+}
