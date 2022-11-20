03f76f9ed0281cdc0437961140a24c05bf43d76d
david-navruz <selcuk-nevruz@hotmail.com>
2022-11-20 17:26:23 +0100

    updating the project

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,23 +9,12 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes:Recipe[] = [
-    new Recipe('Cheese Pizza', 'How to make a cheese pizza',
-              'https://www.bettybossi.ch/rdbimg/bb_itku120801_0243a/bb_itku120801_0243a_r01_v005_x0010.jpg'
-              ),
-    new Recipe('Chicken Pizza', 'How to make a chicken pizza',
-                  'https://www.bettybossi.ch/rdbimg/bb_itku120801_0243a/bb_itku120801_0243a_r01_v005_x0010.jpg'
-                  ),
-  ];
+  recipes:Recipe[];
 
-  constructor() { }
+  constructor(private recipeService:RecipeService) { }
 
   ngOnInit(): void {
-  }
-
-  onRecipeSelected(recipe:Recipe) {
-    this.recipeWasSelected.emit(recipe);
+    this.recipes = this.recipeService.getRecipes();
   }
 
 }
--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,13 +1,15 @@
+import { Ingredient } from '../shared/ingredient.model';
+
 export class Recipe {
+  public name: string;
+  public description: string;
+  public imagePath: string;
+  public ingredients: Ingredient[];
 
-  public recipeName:string;
-  public description:string;
-  public imagePath:string;
-
-  constructor(recipeName:string, description:string, imagePath:string){
-    this.recipeName = recipeName;
-    this.description = description;
+  constructor(name: string, desc: string, imagePath: string, ingredients: Ingredient[]) {
+    this.name = name;
+    this.description = desc;
     this.imagePath = imagePath;
+    this.ingredients = ingredients;
   }
-
 }
