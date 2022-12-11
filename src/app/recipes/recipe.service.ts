ff7a5d754f3c5f0071bf9f39c17c398337e2eaff
david-navruz <selcuk-nevruz@hotmail.com>
2022-12-12 00:35:40 +0100

    updating the project

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -30,10 +30,12 @@ export class RecipeService {
       return this.recipes.slice();
     }
 
+    getRecipe(index: number) {
+       return this.recipes[index];
+    }
+
     addIngredientsToShoppingList(ingredients: Ingredient[]) {
       this.slService.addIngredients(ingredients);
     }
-
-
 
 }
